3ee66afade09e50638f91359429700ca1dfc8c54
kaylalessard <69610614+kaylalessard@users.noreply.github.com>
2021-02-16 13:11:46 -0500

    Update App.js

--- LetsEat/src/App.js
+++ LetsEat/src/App.js
@@ -1,43 +1,36 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-import CreateMeal from "./components/create-meal.component";
+// import CreateMeal from "./components/create-meal.component";
 import Meal from "./components/meal.component";
 import Kids from "./components/kids.component";
 
 
 function App() {
-  return (
+return (
+
+<Router>
   
-  <Router>
-<div className="App">
 <div className="header">
-  {/* header */}
-  <h1>Lets Eat</h1>
+<h1>Lets Eat</h1>
 </div>
+
 {/* buttons direct to data table of adult meals and kids meals */}
 <div className="buttons">
 <Link className="button" to="/meal">Adult Meal</Link>
 <Link className="button" to="/kids">Kids Meal</Link>
-
 </div>
-    {/* this defines our navigation path */}
-      <div className="content">
-        <div className="row">
-          <div className="col-md-12">
-            <Switch>
-              <Route exact path='/' component={CreateMeal} />
-              <Route path="/meal" component={Meal} />
-              <Route path="/kids" component={Kids} />
 
-            </Switch>
-          </div>
-        </div>
-      </div>
-    </div>
-  </Router>
-  );
+{/* this defines our navigation path */}
+<div>
+<Switch>
+{/* <Route exact path='/' component={CreateMeal} /> */}
+<Route path="/meal" component={Meal} />
+<Route path="/kids" component={Kids} />
+</Switch>
+</div>
+</Router>
+);
 }
 
 export default App;
