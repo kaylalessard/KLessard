95d92886204dfbd868405b683d1750fb77351790
kaylalessard <69610614+kaylalessard@users.noreply.github.com>
2021-02-16 13:11:12 -0500

    Update data-table.js

--- LetsEat/src/components/data-table.js
+++ LetsEat/src/components/data-table.js
@@ -2,17 +2,14 @@ import React, { Component } from 'react';
 
 // DataTable class simply displays the meal object from collections
 class DataTable extends Component {
+
+    
     render() {
         return (
-            <tr>
-                <td>
-                    {this.props.obj.meal}
-                </td>
-                
 
-            </tr>
+                    <li>{this.props.obj.meal}</li>
         );
     }
 }
 
-export default DataTable;
+export default DataTable; 
