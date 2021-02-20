b9ad09c28aaf3fd0f7038bbf55a0dcef75bddd7c
kaylalessard <69610614+kaylalessard@users.noreply.github.com>
2021-02-20 18:13:35 -0500

    Update meal.routes.js

--- LetsEat/server/routes/meal.routes.js
+++ LetsEat/server/routes/meal.routes.js
@@ -4,20 +4,8 @@ let mongoose = require('mongoose'),
 
 let meal = require('../models/meal-schema');
 
-// kids route defines our navigation for CRUD operations in the kids collections
+// kids route defines our navigation for GET operations in the kids collections
 
-//create route
-router.route('/create').post((req, res, next) => {
-    meal.create(req.body, (error, data) => {
-        if (error) {
-            return next(error)
-        } else {
-            console.log(data)
-            res.json(data)
-        }
-    })
-});
- 
 router.route('/').get((req, res) => {
     meal.find((error, data) => {
         if (error) {
@@ -28,43 +16,5 @@ router.route('/').get((req, res) => {
     })
 })
 
-//edit route
-router.route('/edit/:id').get((req, res) => {
-    meal.findById(req.params.id, (error, data) => {
-        if (error) {
-            return next(error)
-        } else {
-            res.json(data)
-        }
-    })
-})
-
-
-// Update 
-router.route('/update/:id').put((req, res, next) => {
-    meal.findByIdAndUpdate(req.params.id, {
-        $set: req.body
-    }, (error, data) => {
-        if (error) {
-            return next(error);
-            console.log(error)
-        } else {
-            res.json(data)
-            console.log('Meal updated successfully !')
-        }
-    })
-})
-//delete
-router.route('/delete/:id').delete((req, res, next) => {
-    meal.findByIdAndRemove(req.params.id, (error, data) => {
-        if (error) {
-            return next(error);
-        } else {
-            res.status(200).json({
-                msg: data
-            })
-        }
-    })
-})
 
 module.exports = router;
