9fea04b7bbd06c24cb0951d78082d12a2e516e6b
kaylalessard <69610614+kaylalessard@users.noreply.github.com>
2021-02-20 18:10:40 -0500

    Update kids.component.js

--- LetsEat/src/components/kids.component.js
+++ LetsEat/src/components/kids.component.js
@@ -6,32 +6,68 @@ export default class Kids extends Component {
     constructor(props) {
         super(props);
         this.state = { kidsCollection: [] };
-    }
-
+        this.randomMeal = this.randomMeal.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+  }
+    //get database collection from localhost
     componentDidMount() {
         axios.get('http://localhost:4000/kids')
             .then(res => {
-                this.setState({ kidsCollection: res.data });
+                this.setState({ kidsCollection: res.data },()=>{
+                    // handle click function here, so that a random meal is shown on initial load
+                    this.handleClick();
+                  })
             })
             .catch(function (error) {
                 console.log(error);
             })
+    
     }
-
-
+    //Use Math.floor to find random item in collection
+    randomMeal() {
+        const randomNumber = Math.floor(Math.random() * this.state.kidsCollection.length);
+        return this.state.kidsCollection[randomNumber];
+      }
+        //handleClick function to select random meal
+      handleClick() {
+        const oneRandomMeal = this.randomMeal();
+        this.setState({
+          meal: oneRandomMeal.meal
+        })
+      }
+    
     render() {
         const { kidsCollection } = this.state;
-
         return (
             
-            <div className="text-center">
-
-            {kidsCollection.map(meal =>
-            <div>{meal.meal}</div>)}
- 
-            </div>
-        )
-    }
-    
+        <div className="content">
+            <br></br>
+             <p>
+            {this.state.meal}  
+            </p>
+            <br>
+            </br>
+            <Buttons handleClick={this.handleClick}/>
+        </div>
+        )}
 
 }
+//button class; button click produces random meal from collection
+class Buttons extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+  
+    render() {
+      return(
+    <div className="content">
+        <div class="center">
+          <button className="button" 
+          onClick={this.props.handleClick}>
+            Randomize Meal
+          </button>
+        </div>
+    </div> 
+      )
+    }
+  }
